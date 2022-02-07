09593664e472846a10c9bcc8619ca3ca3b6f2d92
NicolasHup <malulekanicolas@gmail.com>
2022-02-07 10:48:38 +0200

    function that repeats a string a given number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    var repeatedString = "";
+    while (times > 0) {
+        repeatedString = repeatedString + string;
+        times--;
+    }
+    
+    return repeatedString;
 };
+
+repeatString('hey',10);
 
 // Do not edit below this line
 module.exports = repeatString;
