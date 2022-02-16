1921bb95582ac3ea6be9856f31f19e186fed8a67
NicolasHup <malulekanicolas@gmail.com>
2022-02-16 16:13:06 +0200

    added comments on the remove from array functions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,8 @@
 const removeFromArray = function(theArray) {
    
-    for (i = 1; i <= arguments.length -1; i++){
+    for (i = 1; i <= arguments.length -1; i++){// to loop through additional arguments
         let indexToRemove = theArray.indexOf(arguments[i]);
-        if (indexToRemove > - 1) {
+        if (indexToRemove > - 1) {// to check if the index num is positive
             theArray.splice(indexToRemove, 1);
         }
     }
