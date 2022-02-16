789b91e9f2a8366997c057da82db60b1745d551e
NicolasHup <malulekanicolas@gmail.com>
2022-02-16 16:09:42 +0200

    completed the remove from array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(theArray) {
+   
+    for (i = 1; i <= arguments.length -1; i++){
+        let indexToRemove = theArray.indexOf(arguments[i]);
+        if (indexToRemove > - 1) {
+            theArray.splice(indexToRemove, 1);
+        }
+    }
+    return theArray;
 };
 
 // Do not edit below this line
