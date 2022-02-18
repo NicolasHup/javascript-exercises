75290610c45895e1cd6d4061cab255955a8bb6df
NicolasHup <malulekanicolas@gmail.com>
2022-02-18 16:18:50 +0200

    completed sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,34 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    let smallest = a;
+    let biggest = b;
+
+    if (typeof arguments[0] != 'number'){ // checks if the passed argument is a num
+        return 'ERROR';
+    }
+    if (typeof arguments[1] != 'number'){ // checks if the passed argument is a num
+        return 'ERROR';
+    }
+    if (arguments[0] < 0){ // checks if the passed argument is not < 0
+        return 'ERROR';
+    }
+    if (arguments[1] < 0){ // checks if the passed argument is not < 0
+        return 'ERROR';
+    }
+    if (arguments[0] > arguments[1]){ //determines which argument is bigger
+        biggest = arguments[0];
+        smallest = arguments[1];
+    }else{
+        smallest = arguments[0];
+        biggest = arguments[1];
+    }
+
+    for (let i = smallest; i <= biggest; i ++){
+        sum += i;
+
+    }
+
+     return sum;
 
 };
 
