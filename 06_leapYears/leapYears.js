a0efa1cb7d1ffe357d67c95b08c8aad504cea747
NicolasHup <malulekanicolas@gmail.com>
2022-02-25 12:35:45 +0200

    completed leapYear function and and all the tests were passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,32 @@
+// theres three condition: a year divisible by 4 "is a leap yeah"
+                        // 
+                        // a yeah divisible by 100 is not a leap year // unless disible by 400
+
+
 const leapYears = function() {
 
+
+    if (arguments[0] % 4 === 0 && arguments[0] % 100 === 0 && arguments[0] % 400 === 0){
+        return true;
+    } 
+    if (arguments[0] % 4 === 0 && arguments[0] % 100 === 0) {
+        return false;
+    } 
+    if (arguments[0] % 4 === 0) {
+        return true;
+    } 
+    if (arguments[0] % 400 === 0) {
+        return true;
+    } 
+  
+   if (arguments[0] % 100 === 0){
+        return false;
+    }
+     else {
+        return false;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
