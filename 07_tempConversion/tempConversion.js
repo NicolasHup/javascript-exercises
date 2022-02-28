b84baba75edae59cebbc80aee6a4b8557bd494b8
NicolasHup <malulekanicolas@gmail.com>
2022-02-28 09:56:41 +0200

    added new comments

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,12 @@
 const ftoc = function() {
   let calc = (arguments[0]-32)* 5/9;
-  let rounded = Math.round(calc * 10) / 10;
+  let rounded = Math.round(calc * 10) / 10; // round to one decimal place
   return rounded;
 };
 
 const ctof = function() {
   let calc = (arguments[0])* 9/5 + 32;
-  let rounded = Math.round(calc * 10) / 10;
+  let rounded = Math.round(calc * 10) / 10; // round to one decimal place
   return rounded; 
 };
 
