88c0d07782fe39744cf00c444bd5187a9cc91801
NicolasHup <malulekanicolas@gmail.com>
2022-02-28 09:53:17 +0200

    completed the conversion functions & all test went well

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
 const ftoc = function() {
-
+  let calc = (arguments[0]-32)* 5/9;
+  let rounded = Math.round(calc * 10) / 10;
+  return rounded;
 };
 
 const ctof = function() {
-
+  let calc = (arguments[0])* 9/5 + 32;
+  let rounded = Math.round(calc * 10) / 10;
+  return rounded; 
 };
 
 // Do not edit below this line
